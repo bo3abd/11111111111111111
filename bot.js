417f6faaafe45b0b8fcb8bf528c74c0cd044a173
bo3abd <37047946+bo3abd@users.noreply.github.com>
2018-08-09 15:14:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,10 +11,6 @@ const client = new Discord.Client();
 
 
 
-const Discord = require("discord.js");
-const client = new Discord.Client();
-
-
 
 client.on('ready', () => {
         console.log('By !EtWrk.');
